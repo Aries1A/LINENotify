ddf04f05822a2762fe277f0d4335408a0c4a3ff0
Yuya Munekata <yuya@YuyanoMacBook-puro.local>
2018-11-30 01:45:44 +0900

    first commit

--- linenotify.py
+++ linenotify.py
@@ -5,6 +5,6 @@ url = "https://notify-api.line.me/api/notify"
 headers = {'Authorization': 'Bearer ' + ACCESS_TOKEN} # 発行したトークン
 message =  'ここにメッセージを入れます'
 payload = {"message" :  message}
-files = {"imageFile": open("1.jpg", "rb")} #バイナリで画像ファイルを開きます。対応している形式はPNG/JPEGです。
+files = {"imageFile": open("img/1.jpg", "rb")} #バイナリで画像ファイルを開きます。対応している形式はPNG/JPEGです。
 
 r = requests.post(url ,headers = headers ,params=payload, files=files)
